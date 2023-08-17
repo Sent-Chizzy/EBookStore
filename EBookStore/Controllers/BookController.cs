7de80f70eea44e35551febd734115e6e2450f91f
Godsent Ukomadu <Godsentukomadu@outlook.com>
2023-08-18 00:07:38 +0100

    Add project files.

---
+++ EBookStore/Controllers/BookController.cs
@@ -0,0 +1,93 @@
+﻿using EBookStore.Models.Domain;
+using EBookStore.Repositories.Abstract;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace EBookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        private readonly IBookService bookservice;
+        private readonly IAuthorService authorService;
+        private readonly IGenreService genreService;
+        private readonly IPublisherService publisherService;
+
+        public BookController(IBookService bookservice, IPublisherService publisherService,IAuthorService authorService, IGenreService genreService)
+        {
+            this.bookservice = bookservice;
+            this.publisherService = publisherService;
+            this.genreService = genreService;
+            this.authorService = authorService;
+        }
+        public IActionResult Add()
+        {
+            var model = new Book();
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString() }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString() }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString() }).ToList();
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Add(Book model)
+        {
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(),Selected =a.Id == model.AuthorId }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected =a.Id == model.PublisherId }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GenreId }).ToList();
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = bookservice.Add(model);
+            if (result)
+            {
+                TempData["msg"] =  $"{model.Title} Added Successfully";
+                return RedirectToAction(nameof(Add));
+            }
+            TempData["msg"] = "Server Side Error";
+            return View(model);
+        }
+
+        public IActionResult Update(int id)
+        {
+            var model = bookservice.FindById(id);
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(), Selected = a.Id == model.AuthorId }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected = a.Id == model.PublisherId }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GenreId }).ToList();
+            return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Update(Book model)
+        {
+            model.AuthorList = authorService.GetAll().Select(a => new SelectListItem { Text = a.AuthorName, Value = a.Id.ToString(), Selected = a.Id == model.AuthorId }).ToList();
+            model.PublisherList = publisherService.GetAll().Select(a => new SelectListItem { Text = a.PublisherName, Value = a.Id.ToString(), Selected = a.Id == model.PublisherId }).ToList();
+            model.GenreList = genreService.GetAll().Select(a => new SelectListItem { Text = a.Name, Value = a.Id.ToString(), Selected = a.Id == model.GenreId }).ToList();
+            if (!ModelState.IsValid)
+            {
+                return View(model);
+            }
+            var result = bookservice.Update(model);
+            if (result)
+            {
+                return RedirectToAction("GetAll");
+            }
+            TempData["msg"] = "Server Side Error";
+            return View(model);
+        }
+
+        public IActionResult Delete(int id)
+        {
+            var result = bookservice.Delete(id);
+            return RedirectToAction("GetAll");
+        }
+
+        public IActionResult GetAll()
+        {
+            var data = bookservice.GetAll();
+            return View(data);
+        }
+
+    }
+}
---
+++ EBookStore/Migrations/20230815142410_init-updated.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace EBookStore.Migrations
+{
+    public partial class initupdated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres");
+
+            migrationBuilder.RenameTable(
+                name: "Genres",
+                newName: "Genre");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre",
+                column: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Genre",
+                table: "Genre");
+
+            migrationBuilder.RenameTable(
+                name: "Genre",
+                newName: "Genres");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Genres",
+                table: "Genres",
+                column: "Id");
+        }
+    }
+}
